ed816759f90fa0873217da5c87219e86199a504e
federzvz <85709865+federzvz@users.noreply.github.com>
2022-10-10 22:01:23 -0300

    Default Objects

--- project-futchibal/Assets/PlayerMovementController.cs
+++ project-futchibal/Assets/PlayerMovementController.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    public float speed = 1f;
+    public Rigidbody m_RigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_RigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float xDirection = Input.GetAxis("Horizontal");
         float zDirection = Input.GetAxis("Vertical");
 
         Vector3 moveDIrection = new Vector3(xDirection, 0.0f, zDirection);
 
-        transform.position += moveDIrection * 0.03f;
+        transform.position += moveDIrection * speed;
     }
 }
